2c2743d9868f90bf9f04acafcef17f5fcf1e143a
Beteywn <mebrahtubereket@gmail.com>
2021-12-07 04:53:27 +0000

    index.html

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const slidesEl = document.querySelector('.slides');
-const prevbtn = document.querySelector('prev-Btn');
-const nextbtn = document.querySelector('.next-Btn')
+const slides = document.querySelectorAll('.slides');
+const prevbutton = document.querySelector('.prev-Btn');
+const nextbutton = document.querySelector('.next-Btn')
 
 
-slides.forEach(function(slides, index) {
-    slidesEl.style.left = `${index * 100}%`
+slides.forEach(function(slide, index) {
+    slide.style.left = `${index * 100}%`
 });
 
 let counter = 0;
 
-nextbtn.addEventListener('click', () => {
+nextbutton.addEventListener('click', function(){
     counter++;
+
     carousel()
 });
 
-prevbtn.addEventListener('click', ()=> {
+prevbutton.addEventListener('click', ()=> {
     counter--;
+
     carousel()
 })
 
-function counter() {
-    slidesEl.forEach(function(slides){
-        slides.style.transform = `translateX(-${counter*100}%)`
+function carousel() {
+    slides.forEach(function(slide){
+        slide.style.transform = `translateX(-${counter * 100}%)`
     })
 }
