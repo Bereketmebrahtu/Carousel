461707f3ac36431813c7c44c322d553dfe619358
Beteywn <mebrahtubereket@gmail.com>
2021-12-08 23:35:46 +0000

    javascript logic implimentation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# Carousel" 
+First attempt at a carousel, not fully functional yet, however set up seems all fine, need to go over code
+
--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-const slides = document.querySelectorAll('.slides');
-const prevbutton = document.querySelector('.prev-Btn');
-const nextbutton = document.querySelector('.next-Btn')
+const slidesEl = document.querySelectorAll('.slide');
+// const slideImagesEl = document.querySelectorAll('.slide')
+const prevBtnEl = document.querySelector('.prev-Btn');
+const nextBtnEl = document.querySelector('.next-Btn')
 
 
-slides.forEach(function(slide, index) {
+slidesEl.forEach(function(slide, index) {
     slide.style.left = `${index * 100}%`
 });
 
 let counter = 0;
 
-nextbutton.addEventListener('click', function(){
+nextBtnEl.addEventListener('click', function(){
     counter++;
 
     carousel()
 });
 
-prevbutton.addEventListener('click', ()=> {
+prevBtnEl.addEventListener('click', function(){
     counter--;
 
     carousel()
-})
+});
 
 function carousel() {
-    slides.forEach(function(slide){
+    slidesEl.forEach(function(slide){
         slide.style.transform = `translateX(-${counter * 100}%)`
-    })
+    });
 }
